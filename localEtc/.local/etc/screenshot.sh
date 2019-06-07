a914374d49a9e16857d72e6acd567c7d07c15fc1
RONTheCookie <me@ronthecookie.me>
2019-06-07 17:20:47 +0300

    add rm to scrot script

--- localEtc/.local/etc/screenshot.sh
+++ localEtc/.local/etc/screenshot.sh
@@ -5,3 +5,4 @@ gnome-screenshot -apf /tmp/lscr.png
 
 echo -n https://i.ronthecookie.me/$(curl -F file=@/tmp/lscr.png -X POST "https://i.ronthecookie.me/upload/$(cat screenshotToken)" | cut -d \" -f 8) | xclip -selection c
 notify-send "screenshot taken" ""
+rm /tmp/lscr.png
