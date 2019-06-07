b41d9c0963a06e4a1686d517bcfb5a65fe721301
RONTheCookie <me@ronthecookie.me>
2019-06-07 17:19:42 +0300

    add to i3 and improve script

--- localEtc/.local/etc/screenshot.sh
+++ localEtc/.local/etc/screenshot.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 # yay cool script that works like sharenix
+cd ~/.local/etc
 gnome-screenshot -apf /tmp/lscr.png
 
-echo https://i.ronthecookie.me/$(curl -F file=@/tmp/lscr.png -X POST "https://i.ronthecookie.me/upload/$(cat screenshotToken)" | cut -d \" -f 8) | xclip -selection c
+echo -n https://i.ronthecookie.me/$(curl -F file=@/tmp/lscr.png -X POST "https://i.ronthecookie.me/upload/$(cat screenshotToken)" | cut -d \" -f 8) | xclip -selection c
+notify-send "screenshot taken" ""
