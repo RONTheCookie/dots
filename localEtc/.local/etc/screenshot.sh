a14d29f38a6438da0c644007694fcc0768500a53
RONTheCookie <me@ronthecookie.me>
2019-10-03 17:59:14 +0300

    sway basic setup

--- localEtc/.local/etc/screenshot.sh
+++ localEtc/.local/etc/screenshot.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 # yay cool script that works like sharenix
 cd ~/.local/etc
-gnome-screenshot -apf /tmp/lscr.png
+grim -g "$(slurp)" /tmp/lscr.png
 
-echo -n https://i.ronthecookie.me/$(curl -F file=@/tmp/lscr.png -X POST "https://i.ronthecookie.me/upload/$(cat screenshotToken)" | cut -d \" -f 8) | xclip -selection c
+echo -n https://i.ronthecookie.me/$(curl -F file=@/tmp/lscr.png -X POST "https://i.ronthecookie.me/upload/$(cat screenshotToken)" | cut -d \" -f 8) | wl-copy
 notify-send "screenshot taken" ""
 rm /tmp/lscr.png
