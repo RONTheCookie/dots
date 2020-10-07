130ccd7ce10b7dfb30d459c54c2987e1c92145bc
Ron B <me@ronthecookie.me>
2020-10-07 18:22:11 +0300

    organize

--- localEtc/.local/etc/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ~/.local/etc
-xss-lock -n "slock" -- systemctl suspend
-# sh ./occasionallychangebg.sh & It used to be this: wget -O bg $(curl https://bing.biturl.top/ | grep -Po '"url":.*?[^\\]",' | sed 's/"url":"//g' | sed 's/",//g')
-# But that's insecure.
